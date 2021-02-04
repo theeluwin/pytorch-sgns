9548ae5b164de73a141641fcc504f751e0c7dde2
keren <keren@vulcan.io>
2021-02-04 18:13:19 +0200

    delete create corpus

--- prepare_corpus.py
+++ prepare_corpus.py
@@ -19,14 +19,8 @@ def extract_corpus(u_lsts):
             the_file.write(sent + '\n')
 
 
-def prepare_corpus():
+if __name__ == '__main__':
     data = pd.read_csv(DATA_FPATH, delimiter='\t', names=DATA_COLS)
     data = filter_positives(data)
     u_lsts = build_u_lsts(data)
     extract_corpus(u_lsts)
-
-# if __name__ == '__main__':
-#     data = pd.read_csv(DATA_FPATH, delimiter='\t', names=DATA_COLS)
-#     data = filter_positives(data)
-#     u_lsts = build_u_lsts(data)
-#     extract_corpus(u_lsts)
