2aa238ad688d224ed3a60100371765c6589d35da
keren <keren@vulcan.io>
2021-02-10 08:24:25 +0200

    create train_evaluate function with all its sub function for the hyper params tuning

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import numpy as np
 from tqdm import tqdm
 from torch.optim import Adam
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 from model import Word2Vec, SGNS
 
 
@@ -23,9 +24,11 @@ def parse_args():
     parser.add_argument('--epoch', type=int, default=100, help="number of epochs")
     parser.add_argument('--mb', type=int, default=4096, help="mini-batch size")
     parser.add_argument('--ss_t', type=float, default=1e-5, help="subsample threshold")
+    parser.add_argument('--valid_split', type=float, default=0.22, help="validation rate")
     parser.add_argument('--conti', action='store_true', help="continue learning")
     parser.add_argument('--weights', action='store_true', help="use weights for negative sampling")
     parser.add_argument('--cuda', action='store_true', help="use CUDA")
+
     return parser.parse_args()
 
 
@@ -57,7 +60,7 @@ def train(args):
     ws = 1 - np.sqrt(args.ss_t / wf)
     ws = np.clip(ws, 0, 1)
     vocab_size = len(idx2word)
-    weights = wf if args.weights else None
+    weights = ws if args.weights else None
     if not os.path.isdir(args.save_dir):
         os.mkdir(args.save_dir)
     model = Word2Vec(vocab_size=vocab_size, embedding_size=args.e_dim)
@@ -87,6 +90,82 @@ def train(args):
     pickle.dump(idx2vec, open(os.path.join(args.data_dir, 'idx2vec.dat'), 'wb'))
     t.save(sgns.state_dict(), os.path.join(args.save_dir, '{}.pt'.format(args.name)))
     t.save(optim.state_dict(), os.path.join(args.save_dir, '{}.optim.pt'.format(args.name)))
+
+
+def run_epoch(train_dl, epoch, model, optim):
+    pbar = tqdm(train_dl)
+    pbar.set_description("[Epoch {}]".format(epoch))
+    for iword, owords in pbar:
+        loss = model(iword, owords)
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        pbar.set_postfix(train_loss=loss.item())
+
+
+def evaluate(valid_dl, model):
+    loss_lst = []
+    pbar = tqdm(valid_dl)
+    for iword, owords in pbar:
+        loss = model(iword, owords)
+        loss_lst.append(loss)
+
+    return np.array(loss_lst).mean()
+
+
+def split_train_test(args, cnfg, ws):
+    dataset = PermutedSubsampledCorpus(os.path.join(args.data_dir, 'train.dat'), ws)
+    # Creating data indices for training and validation splits:
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(args.validation_split * dataset_size))
+    train_indices, val_indices = indices[split:], indices[:split]
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_loader = DataLoader(dataset, batch_size=cnfg['mini_batch'], sampler=train_sampler)
+    validation_loader = DataLoader(dataset, batch_size=cnfg['mini_batch'], sampler=valid_sampler)
+    return train_loader, validation_loader
+
+
+def train_evaluate(cnfg, args):
+    idx2word = pickle.load(open(os.path.join(args.data_dir, 'idx2word.dat'), 'rb'))
+    wc = pickle.load(open(os.path.join(args.data_dir, 'wc.dat'), 'rb'))
+    wf = np.array([wc[word] for word in idx2word])
+    wf = wf / wf.sum()
+    ws = 1 - np.sqrt(args.ss_t / wf)
+    ws = np.clip(ws, 0, 1)
+    vocab_size = len(idx2word)
+    weights = ws if cnfg['weights'] else None
+
+    if not os.path.isdir(args.save_dir):
+        os.mkdir(args.save_dir)
+
+    model = Word2Vec(vocab_size=vocab_size, embedding_size=cnfg['e_dim'])
+    sgns = SGNS(embedding=model, vocab_size=vocab_size, n_negs=cnfg['n_negs'], weights=weights)
+
+    modelpath = os.path.join(args.save_dir, '{}.pt'.format(args.name))
+    if os.path.isfile(modelpath) and args.conti:
+        sgns.load_state_dict(t.load(modelpath))
+
+    if args.cuda:
+        sgns = sgns.cuda()
+
+    optim = Adam(sgns.parameters(), lr=cnfg['lr'])
+    optimpath = os.path.join(args.save_dir, '{}.optim.pt'.format(args.name))
+    if os.path.isfile(optimpath) and args.conti:
+        optim.load_state_dict(t.load(optimpath))
+
+    train_loader, validation_loader = split_train_test(args, cnfg, ws)
+
+    for epoch in range(1, cnfg['epoch'] + 1):
+        run_epoch(train_loader, epoch, sgns, optim)
+        # TODO: implement early stopping, calculate the loss/ specific evaluation metrics on validation
+        #  in each epoch and stop when it stuck.
+
+    mean_loss_val = evaluate(validation_loader, model)
+    return mean_loss_val
 
 
 if __name__ == '__main__':
