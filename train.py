49ce33cf1b3c51e59e38463a63fc8194e65673ea
keren <keren@vulcan.io>
2021-02-15 20:14:38 +0200

    Train: Delete obsolete code

--- train.py
+++ train.py
@@ -38,46 +38,6 @@ class PermutedSubsampledCorpus(Dataset):
         return iword, np.array(owords)
 
 
-def train(args):
-    idx2word = pickle.load(open(os.path.join(args.data_dir, 'idx2word.dat'), 'rb'))
-    wc = pickle.load(open(os.path.join(args.data_dir, 'wc.dat'), 'rb'))
-    wf = np.array([wc[word] for word in idx2word])
-    wf = wf / wf.sum()
-    ws = 1 - np.sqrt(args.ss_t / wf)
-    ws = np.clip(ws, 0, 1)
-    vocab_size = len(idx2word)
-    weights = ws if args.weights else None
-    if not os.path.isdir(args.save_dir):
-        os.mkdir(args.save_dir)
-    model = Word2Vec(vocab_size=vocab_size, embedding_size=args.e_dim)
-    modelpath = os.path.join(args.save_dir, '{}.pt'.format(args.name))
-    sgns = SGNS(embedding=model, vocab_size=vocab_size, n_negs=args.n_negs, weights=weights)
-    if os.path.isfile(modelpath) and args.conti:
-        sgns.load_state_dict(t.load(modelpath))
-    if args.cuda:
-        sgns = sgns.cuda()
-    optim = Adam(sgns.parameters())
-    optimpath = os.path.join(args.save_dir, '{}.optim.pt'.format(args.name))
-    if os.path.isfile(optimpath) and args.conti:
-        optim.load_state_dict(t.load(optimpath))
-    for epoch in range(1, args.epoch + 1):
-        dataset = PermutedSubsampledCorpus(os.path.join(args.data_dir, 'train.dat'))
-        dataloader = DataLoader(dataset, batch_size=args.mb, shuffle=True)
-        total_batches = int(np.ceil(len(dataset) / args.mb))
-        pbar = tqdm(dataloader)
-        pbar.set_description("[Epoch {}]".format(epoch))
-        for iword, owords in pbar:
-            loss = sgns(iword, owords)
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-            pbar.set_postfix(loss=loss.item())
-    idx2vec = model.ivectors.weight.data.cpu().numpy()
-    pickle.dump(idx2vec, open(os.path.join(args.data_dir, 'idx2vec.dat'), 'wb'))
-    t.save(sgns.state_dict(), os.path.join(args.save_dir, '{}.pt'.format(args.name)))
-    t.save(optim.state_dict(), os.path.join(args.save_dir, '{}.optim.pt'.format(args.name)))
-
-
 def run_epoch(train_dl, epoch, sgns, optim):
     pbar = tqdm(train_dl)
     pbar.set_description("[Epoch {}]".format(epoch))
@@ -87,16 +47,6 @@ def run_epoch(train_dl, epoch, sgns, optim):
         loss.backward()
         optim.step()
         pbar.set_postfix(train_loss=loss.item())
-
-
-def evaluate(valid_dl, model):
-    loss_lst = []
-    pbar = tqdm(valid_dl)
-    for iword, owords in pbar:
-        loss = model(iword, owords)
-        loss_lst.append(loss)
-
-    return np.array(loss_lst).mean()
 
 
 def train_to_dl(mini_batch_size):
@@ -147,6 +97,3 @@ def train_evaluate(cnfg):
 
     return last_epoch_perf
 
-#
-# if __name__ == '__main__':
-#     train(parse_args())
