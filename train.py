ad51878fbfc191f94baa29d8edcb3829b0a93d1a
keren <keren@vulcan.io>
2021-02-18 12:41:02 +0200

    Train: seperate to train, evaluate and train evaluate

--- train.py
+++ train.py
@@ -14,6 +14,8 @@ from tqdm import tqdm
 from evaluation import users2items, hr_k, mrr_k
 from model import Item2Vec, SGNS
 
+import matplotlib.pyplot as plt
+
 
 class PermutedSubsampledCorpus(Dataset):
 
@@ -50,6 +52,7 @@ def run_epoch(train_dl, epoch, sgns, optim):
 
     train_loss = np.array(train_losses).mean()
     print(f'train_loss: {train_loss}')
+    return train_loss
 
 
 def train_to_dl(mini_batch_size, train_path):
@@ -70,8 +73,38 @@ def configure_weights(cnfg, idx2item):
     return weights
 
 
-def train_evaluate(cnfg):
-    print(cnfg)
+def train(cnfg):
+    idx2item = pickle.load(pathlib.Path(cnfg['data_dir'], 'idx2item.dat').open('rb'))
+
+    weights = configure_weights(cnfg, idx2item)
+    vocab_size = len(idx2item)
+
+    model = Item2Vec(vocab_size=vocab_size, embedding_size=cnfg['e_dim'])
+    sgns = SGNS(embedding=model, vocab_size=vocab_size, n_negs=cnfg['n_negs'], weights=weights)
+    if cnfg['cuda']:
+        sgns = sgns.cuda()
+
+    optim = Adagrad(sgns.parameters(), lr=cnfg['lr'])
+
+    train_loader = train_to_dl(cnfg['mini_batch'],
+                               pathlib.Path(cnfg['data_dir'], 'train.dat'))
+    for epoch in range(1, cnfg['max_epoch'] + 1):
+        _train_loss = run_epoch(train_loader, epoch, sgns, optim)
+
+    ivectors = model.ivectors.weight.data.cpu().numpy()
+    ovectors = model.ovectors.weight.data.cpu().numpy()
+    pickle.dump(ivectors, open(pathlib.Path(cnfg['save_dir'], 'idx2ivec.dat'), 'wb'))
+    pickle.dump(ovectors, open(pathlib.Path(cnfg['save_dir'], 'idx2ovec.dat'), 'wb'))
+    t.save(sgns.state_dict(), pathlib.Path(cnfg['save_dir'], 'sgns.pt'))
+
+
+def evaluate(model, cnfg, user_lsts, eval_set):
+    e_hr_k = hr_k(model, cnfg['k'], user_lsts, eval_set)
+    e_mrr_k = mrr_k(model, cnfg['k'], user_lsts, eval_set)
+    return e_hr_k * cnfg['hrk_weight'] + e_mrr_k * (1 - cnfg['hrk_weight'])
+
+
+def train_early_stop(cnfg, eval_set, user_lsts, plot=True):
     idx2item = pickle.load(pathlib.Path(cnfg['data_dir'], 'idx2item.dat').open('rb'))
 
     weights = configure_weights(cnfg, idx2item)
@@ -87,49 +120,54 @@ def train_evaluate(cnfg):
     train_loader = train_to_dl(cnfg['mini_batch'],
                                pathlib.Path(cnfg['data_dir'], 'train.dat'))
 
-    last_epoch_perf = -np.inf
     early_stop_epoch = cnfg['max_epoch'] + 1
+    valid_accs = []
+    train_losses = []
+    patience_count = 0
+    best_model = None
 
-    # early_stopping = EarlyStopping(
-    #     monitor='val_accuracy',
-    #     min_delta=0.00,
-    #     patience=3,
-    #     verbose=False,
-    #     mode='max'
-    # )
     for epoch in range(1, cnfg['max_epoch'] + 1):
-        run_epoch(train_loader, epoch, sgns, optim)
-        if cnfg['valid']:
-            user_lsts = users2items(pathlib.Path(cnfg['data_dir'], 'item2idx.dat'),
-                                    pathlib.Path(cnfg['data_dir'], 'vocab.dat'),
-                                    pathlib.Path(cnfg['data_dir'], 'train_corpus.txt'))
-            eval_set = pd.read_csv(pathlib.Path(cnfg['data_dir'], 'valid.txt'))
-            e_hr_k = hr_k(model, cnfg['k'], user_lsts, eval_set)
-            e_mrr_k = mrr_k(model, cnfg['k'], user_lsts, eval_set)
-            perf = e_hr_k * cnfg['hrk_weight'] + e_mrr_k * (1 - cnfg['hrk_weight'])
-            perf_diff = perf - last_epoch_perf
+        train_loss = run_epoch(train_loader, epoch, sgns, optim)
+        train_losses.append(train_loss)
+        valid_acc = evaluate(model, cnfg, user_lsts, eval_set)
+        print(f'valid acc:{valid_acc}')
 
-            # # early_stopping needs the validation loss to check if it has decresed,
-            # # and if it has, it will make a checkpoint of the current model
-            # early_stopping(perf, model)
-            #
-            # if early_stopping.early_stop:
-            #     early_stop_epoch = epoch
-            #     print("Early stopping")
-            #     break
-            print(f'valid acc:{perf}')
-            if perf_diff < cnfg['conv_thresh']:
-                early_stop_epoch = epoch
-                print(f"Early stop at epoch:{epoch}")
-                break
+        patience_count += int(valid_acc - valid_accs[-1] < cnfg['conv_thresh'])
+        if patience_count == cnfg['patience']:
+            early_stop_epoch = epoch
+            print(f"Early stop at epoch:{epoch}")
+            break
 
-            last_epoch_perf = perf
+        if valid_acc > valid_accs[-1]:
+            best_model = model
+        valid_accs.append(valid_acc)
 
-    if 'save_dir' in cnfg.keys():
-        ivectors = model.ivectors.weight.data.cpu().numpy()
-        ovectors = model.ovectors.weight.data.cpu().numpy()
-        pickle.dump(ivectors, open(pathlib.Path(cnfg['save_dir'], 'idx2ivec.dat'), 'wb'))
-        pickle.dump(ovectors, open(pathlib.Path(cnfg['save_dir'], 'idx2ovec.dat'), 'wb'))
-        t.save(sgns.state_dict(), pathlib.Path(cnfg['save_dir'], 'sgns.pt'))
-    return {'0.5*hr_k + 0.5*mrr_k': (last_epoch_perf, 0.0), 'early_stop_epoch': (early_stop_epoch, 0.0)}
+    if plot:
+        fig, ax = plt.subplots(constrained_layout=True)
+
+        ax.plot(range(len(train_losses)), train_losses, label="train_loss")
+        ax.plot(range(len(valid_accs)), valid_accs, label="valid_acc")
+        ax.xlabel('epochs')
+
+        ax.set_ylabel(r'train_loss')
+        secaxx = ax.secondary_xaxis('top')
+        secaxx.set_xlabel('valid_acc')
+
+        plt.title('Train loss - Valid accuracy')
+        # show a legend on the plot
+        plt.legend()
+        fig.savefig(f'plot_{str(cnfg["lr"])}')
+
+    return best_model, early_stop_epoch
+
+
+def train_evaluate(cnfg):
+    user_lsts = users2items(pathlib.Path(cnfg['data_dir'], 'item2idx.dat'),
+                            pathlib.Path(cnfg['data_dir'], 'vocab.dat'),
+                            pathlib.Path(cnfg['data_dir'], 'train_corpus.txt'))
+    eval_set = pd.read_csv(pathlib.Path(cnfg['data_dir'], 'valid.txt'))
+    best_model, early_stop_epoch = train_early_stop(cnfg, eval_set, user_lsts, plot=True)
+
+    acc = evaluate(best_model, cnfg, user_lsts, eval_set)
+    return {'0.5*hr_k + 0.5*mrr_k': (acc, 0.0), 'early_stop_epoch': (early_stop_epoch, 0.0)}
 
