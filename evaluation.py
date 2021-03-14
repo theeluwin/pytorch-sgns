c470460832688fe3c06dcb56816c0e3d185f2539
Ofekkir <ofekkir@gmail.com>
2021-03-14 07:58:14 +0200

    Evaluation: turn target_item to target_item_idx

--- evaluation.py
+++ evaluation.py
@@ -14,7 +14,7 @@ def _extract_item_ids(corpus_line, vocab, item2idx, unk_token):
         ]
 
 
-def users2items(item2index_path, vocab_path, corpus_path, unk_token):
+def users2itemids(item2index_path, vocab_path, corpus_path, unk_token):
     users = []
     item2idx = pickle.load(open(item2index_path, 'rb'))
     vocab = pickle.load(open(vocab_path, 'rb'))
@@ -30,17 +30,18 @@ def users2items(item2index_path, vocab_path, corpus_path, unk_token):
     return users
 
 
-def represent_user(user_items, model):
+def represent_user(user_itemids, model):
     context_vecs = model.ivectors.weight.data.cpu().numpy()
-    user2vec = context_vecs[user_items, :].mean(axis=0)
+    user2vec = context_vecs[user_itemids, :].mean(axis=0)
     return user2vec
 
 
-def hr_k(model, k, users2items, eval_set):
+def hr_k(model, k, users2itemids, eval_set, item2idx, unk):
     in_top_k = 0
     for u_id, target_item in eval_set[['user_id', 'item_id']].values:
-        top_k_items = _calc_item_rank(k, model, u_id, users2items)
-        if target_item in top_k_items:
+        target_item_idx = item2idx.get(target_item, unk)
+        top_k_item_idxs = _calc_item_rank(k, model, u_id, users2itemids)
+        if target_item_idx in top_k_item_idxs:
             in_top_k += 1
     hr_k = in_top_k / eval_set.shape[0]
     return hr_k
@@ -57,8 +58,8 @@ def mrr_k(model, k, users2items, eval_set):
     return mrp_k
 
 
-def _calc_item_rank(k, model, u_id, users2items):
-    user2vec = represent_user(users2items[u_id], model).reshape(1, -1)
+def _calc_item_rank(k, model, u_id, users2itemids):
+    user2vec = represent_user(users2itemids[u_id], model).reshape(1, -1)
     user_sim = cosine_similarity(user2vec, model.ovectors.weight.data.cpu().numpy()).squeeze()
     top_k_items = user_sim.argsort()[-k:][::-1]
     return top_k_items
